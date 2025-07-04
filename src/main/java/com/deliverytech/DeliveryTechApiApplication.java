30a63265d5d14da0b934ec502d332581b09a2952
Alexandre  Erdmann <erdmannsilva@hotmail.com>
2025-07-04 19:09:39 -0300

    Add files via upload

---
+++ src/main/java/com/deliverytech/DeliveryTechApiApplication.java
@@ -0,0 +1,11 @@
+package com.deliverytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTechApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryTechApiApplication.class, args);
+    }
+}
