7c4fdc3ed9d2ab613b889d2c375abbca496b39b9
alberane <alberanelucio@gmail.com>
2025-07-07 11:35:48 -0300

    gitignore

--- src/main/java/com/deliverytech/config/SecurityConfig.java
+++ src/main/java/com/deliverytech/config/SecurityConfig.java
@@ -29,10 +29,12 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/swagger-ui.html", "/swagger-ui/**", "/api-docs/**", "/h2-console/**", "/index.html", "/static/**" ).permitAll()
+                        .requestMatchers("/api/auth/**", "/swagger-ui.html", "/swagger-ui/**", "/api-docs/**",
+                                "/h2-console/**", "/index.html", "/static/**")
+                        .permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .headers(headers -> headers.frameOptions().sameOrigin())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
